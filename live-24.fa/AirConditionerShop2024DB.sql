1a0028e10337af4b353d6474e6a8314f05db199d
doit-now <hoang.nguyenthe@gmail.com>
2024-11-03 20:23:39 +0700

    upload 24.fa.pe resource

---
+++ live-24.fa/AirConditionerShop2024DB.sql
@@ -0,0 +1,29 @@
+USE AirConditionerShop2024DB
+GO
+
+DELETE FROM AirConditioner
+GO
+
+-- CHUẨN LẠI CSDL ĐỂ ĐỦ ĐA DẠNG MÁY LẠNH VÀ NHÀ CUNG CẤP
+-- 10 MÁY LẠNH, 5 NHÀ CUNG CẤP. MỖI NHÀ CUNG CẤP CÓ 2 MÁY LẠNH
+
+INSERT INTO AirConditioner VALUES(101,N'4-16KW A+++ DC Inverter Monoblock Air Source Heat Pump for Hot Water Home Heating Cooling','3 years',N'53dB(A)','House Heating Cooling and Water Heating', 12, 2978,'SC0005')
+GO
+INSERT INTO AirConditioner VALUES(102,N'Hisense AC 9000btu 12000btu 18000btu 24000btu Cooling Inverter Super Save Energy wall mounted Wifi Home Air Conditioner Factory','5 years',N'18-45dB','Split Wall Mounted Air Conditioners', 10, 4412,'SC0005')
+GO
+INSERT INTO AirConditioner VALUES(103,N'Gree Air To Water Versati III Split Inverter Heat Pump 380v 10kw 12kw 14kw 16kw 18kw','2 years',N'48dB(A)','House Heating Cooling and Water Heating', 15, 3765,'SC0006')
+GO
+INSERT INTO AirConditioner VALUES(104,N'Gree 2022 R32 R410a Split Heatpump 10kw 15kw 20kw Germany Warmepumpe Mini Split Inverter Air Source Heat Pump For Sale','1 years',N'49/50dB(A)','Air Source Heat Pump', 18, 2034,'SC0006')
+GO
+INSERT INTO AirConditioner VALUES(105,N'R290 Monoblock DC EVI Invert Air to Water Heat Pump for Home Central Heating air conditioner Sanitary Hot water','10 years',N'18-60dB','Heating + Hot Water+cooling', 15, 3120,'SC0007')
+GO
+INSERT INTO AirConditioner VALUES(106,N'Competitive Portable Heat Pump Air Conditioner Hot Water Heating Cooling Heat Pumps','6 years',N'44/45dB','Swimming Pool Heating Cooling', 10, 3600,'SC0007')
+GO
+INSERT INTO AirConditioner VALUES(107, N'Lennox Aquarea High Performance Heat Pump 9kW Split Inverter', '5 years', N'46dB(A)', 'House Heating and Cooling', 12, 2890, 'SC0008')
+GO
+INSERT INTO AirConditioner VALUES(108, N'Lennox Therma V R32 Monobloc Split 16kW Air Source Heat Pump', '3 years', N'43/44dB(A)', 'Air to Water Heat Pump', 20, 4250, 'SC0008')
+GO
+INSERT INTO AirConditioner VALUES(109, N'Trane Electric Ecodan R32 14kW Monobloc Air Source Heat Pump', '4 years', N'42dB(A)', 'Central Heating and Cooling', 17, 3999, 'SC0009')
+GO
+INSERT INTO AirConditioner VALUES(110, N'Trane Estia R32 Air to Water Split 11kW Heat Pump', '7 years', N'47/48dB(A)', 'House Heating and Sanitary Hot Water', 13, 3500, 'SC0009')
+GO
